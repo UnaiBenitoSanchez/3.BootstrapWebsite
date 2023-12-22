8d86abe09dfb4610cf44e25adb7fad861d489c64
UnaiBenitoSanchez <unaibenitos@gmail.com>
2023-12-22 19:17:22 +0100

    A

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-// a
+document.addEventListener('DOMContentLoaded', function () {
+    const inventario = {
+        'Producto A': 150,
+        'Producto B': 80,
+    };
+
+    Object.entries(inventario).forEach(([producto, cantidad]) => {
+        if (cantidad < 50) {
+            mostrarAlertaBajoStock(producto, cantidad);
+        }
+    });
+});
+
+function mostrarAlertaBajoStock(producto, cantidad) {
+    alert(`¡Atención! El nivel de stock para ${producto} es bajo (${cantidad} unidades). Considere reabastecer.`);
+}
