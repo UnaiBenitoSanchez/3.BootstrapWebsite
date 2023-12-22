d21712e3e1a5f32c30a85209784604cefbe82852
UnaiBenitoSanchez <unaibenitos@gmail.com>
2023-12-22 19:41:42 +0100

    a

---
+++ obtener_inventario.php
@@ -0,0 +1,28 @@
+<?php
+
+$servername = "localhost";
+$username = "enzo";
+$password = "xd";
+$dbname = "proyectozombozo";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+$sql = "SELECT nombre, cantidad_disponible FROM inventario WHERE cantidad_disponible < 50";
+
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
+
+$conn->close();
+?>
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const inventario = {
-        'Producto A': 150,
-        'Producto B': 80,
-    };
+// scripts/main.js
 
-    Object.entries(inventario).forEach(([producto, cantidad]) => {
-        if (cantidad < 50) {
-            mostrarAlertaBajoStock(producto, cantidad);
+$(document).ready(function () {
+    // Hacer una solicitud AJAX para obtener datos de inventario
+    $.ajax({
+        url: 'obtener_inventario.php', // Ajusta la URL según la configuración de tu servidor
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            // Manejar los datos obtenidos
+            manejarDatosInventario(data);
+        },
+        error: function (error) {
+            console.error('Error al obtener datos de inventario:', error);
         }
     });
 });
 
+function manejarDatosInventario(data) {
+    // Iterar sobre los datos del inventario y mostrar alertas según sea necesario
+    data.forEach(function (producto) {
+        if (producto.cantidad_disponible < 50) {
+            mostrarAlertaBajoStock(producto.nombre, producto.cantidad_disponible);
+        }
+    });
+}
+
 function mostrarAlertaBajoStock(producto, cantidad) {
+    // Muestra una alerta para productos con bajos niveles de stock
     alert(`¡Atención! El nivel de stock para ${producto} es bajo (${cantidad} unidades). Considere reabastecer.`);
 }
