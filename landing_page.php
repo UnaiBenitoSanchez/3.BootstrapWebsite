5aed3b1d118613da63b93f0c814d6ba8eaaf5ac6
UnaiBenitoSanchez <unaibenitos@gmail.com>
2024-01-03 17:49:25 +0100

    Dashboard and landing_page now with NavBar.

--- landing_page.php
+++ landing_page.php
@@ -2,44 +2,55 @@
 <html lang="es">
 
 <head>
-
     <!-- meta -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- css -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 
     <!-- js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="js/dashboard.js"></script>
 
     <!-- title -->
     <title>Inventory management dashboard</title>
+    <style>
+        .navbar-nav .nav-link {
+            color: #ffffff; 
+            transition: color 0.3s; 
+        }
 
+        .navbar-nav .nav-link:hover {
+            color: #000000;
+        }
+    </style>
 </head>
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Bootstrap Website</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Products from your factory<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Products categories</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Your factory</a>
-                </li>
-            </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="landing_page.php" style="color: #ffffff">Dashboard</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="dashboard.html">Products from your factory</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Your factory</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
