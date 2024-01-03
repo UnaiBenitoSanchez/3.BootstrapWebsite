12474198d7fb3479a2805cede3b61c9a980fbd44
UnaiBenitoSanchez <unaibenitos@gmail.com>
2024-01-03 18:53:01 +0100

    Logout added.

--- landing_page.php
+++ landing_page.php
@@ -47,7 +47,7 @@
                         <a class="nav-link" href="#">Your factory</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" href="logout.php">Logout</a>
                     </li>
                 </ul>
             </div>
