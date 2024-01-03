803dd6b8244f02b6c6a6e6e9d9af58c365d42840
UnaiBenitoSanchez <unaibenitos@gmail.com>
2024-01-03 14:11:08 +0100

    Login/Register created and db_connect and getData splited in 2 documents.

--- getData.php
+++ getData.php
@@ -1,14 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "unai";
-$password = "xd";
-$dbname = "bootstrapwebsite";
+require_once 'db_connect.php';
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     $sql = "SELECT p.id_product, p.name, p.description, p.price, i.available_quantity
             FROM product p
             INNER JOIN inventory i ON p.id_product = i.product_id_product";
@@ -22,8 +16,7 @@ try {
 
     echo json_encode($data);
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Error: " . $e->getMessage();
 } finally {
     $conn = null;
 }
-?>
--- js/index.js
+++ js/index.js
@@ -1,45 +1,11 @@
-$(document).ready(function () {
-    $.ajax({
-        url: 'getData.php',
-        method: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            mostrarProductosEnBody(data);
-        },
-        error: function (error) {
-            console.error('Error al obtener datos de productos:', error);
-        }
-    });
+const container = document.querySelector('.container');
+const signupButton = document.querySelector('.signup-section header');
+const loginButton = document.querySelector('.login-section header');
+
+loginButton.addEventListener('click', () => {
+    container.classList.add('active');
 });
 
-function mostrarProductosEnBody(data) {
-    var productosContainer = $('#productos-container');
-
-    productosContainer.empty();
-
-    var totalProductos = 0;
-
-    data.forEach(function (producto) {
-        totalProductos += parseFloat(producto.available_quantity);
-    });
-
-    data.forEach(function (producto) {
-        var cantidadDisponible = parseFloat(producto.available_quantity);
-        var porcentaje = totalProductos !== 0 ? (cantidadDisponible / totalProductos) * 100 : 0;
-
-        var productoDiv = $('<div>').addClass('col-md-4 mb-4');
-        productoDiv.append('<div class="card">\
-                            <div class="card-body">\
-                                <h5 class="card-title">' + producto.name + '</h5>\
-                                <p class="card-text">' + producto.description + '</p>\
-                                <p class="card-text">Cantidad: ' + cantidadDisponible + '</p>\
-                                <div class="progress">\
-                                    <div class="progress-bar" role="progressbar" style="width: ' + porcentaje + '%;" aria-valuenow="' + porcentaje + '" aria-valuemin="0" aria-valuemax="100">\
-                                        <span class="porcentaje-text">' + porcentaje.toFixed(2) + '%</span>\
-                                    </div>\
-                                </div>\
-                            </div>\
-                        </div>');
-        productosContainer.append(productoDiv);
-    });
-}
+signupButton.addEventListener('click', () => {
+    container.classList.remove('active');
+});
