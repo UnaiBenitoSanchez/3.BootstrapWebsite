eaa7f2e8e032508698e57a3c51b1d579eeabc27c
UnaiBenitoSanchez <unaibenitos@gmail.com>
2024-01-09 15:28:19 +0100

    Chart de líneas de prueba añadido a landing page.

--- landing_page.php
+++ landing_page.php
@@ -12,6 +12,31 @@
     <!-- js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
 
     <!-- title -->
     <title>Inventory management dashboard</title>
@@ -21,6 +46,7 @@
 <body>
 
     <?php include './controller/navbar.php'; ?>
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
 
 </body>
 
